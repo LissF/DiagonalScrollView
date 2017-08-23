2e7629779fdacd84f5b292a40b0f4121c39e748a
LissF <Liss_F@ukr.net>
2017-08-23 18:07:22 +0300

    Fix: fillViewport API

--- scroller/src/main/java/ua/org/tenletters/widget/DiagonalScrollView.java
+++ scroller/src/main/java/ua/org/tenletters/widget/DiagonalScrollView.java
@@ -186,13 +186,22 @@ public class DiagonalScrollView extends FrameLayout {
     }
 
     /**
+     * Indicates whether this ScrollView's content is stretched to fill the viewport.
+     *
+     * @return True if the content fills the viewport, false otherwise.
+     */
+    public boolean isFillViewport() {
+        return mFillViewport;
+    }
+
+    /**
      * Indicates this ScrollView whether it should stretch its content height to fill
      * the viewport or not.
      *
      * @param fillViewport True to stretch the content's height to the viewport's
      *        boundaries, false otherwise.
      */
-    private void setFillViewport(boolean fillViewport) {
+    public void setFillViewport(boolean fillViewport) {
         if (fillViewport != mFillViewport) {
             mFillViewport = fillViewport;
             requestLayout();
